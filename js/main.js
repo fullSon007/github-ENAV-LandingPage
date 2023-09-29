0e647f00cc9a65b6fd8116177bc9e3b74c095f82
Đào Thái Sơn <thaisonn0701@gmail.com>
2023-09-29 22:48:16 +0700

    scrolltoTop Btn

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+// slide navi
 let currentSlide = 0;
 const slides = document.querySelectorAll('.service__slide');
 const orders = document.querySelectorAll('.slide-item')
@@ -15,7 +16,7 @@ function showSlide(n) {
   });
 
   orders.forEach((order, index) => {
-    if(index === currentSlide) {
+    if (index === currentSlide) {
       order.classList.add('slide-item--active');
     }
     else order.classList.remove('slide-item--active');
@@ -33,3 +34,26 @@ function prevSlide() {
 }
 
 showSlide(currentSlide);
+
+// scrolltoTop
+
+// Lắng nghe sự kiện cuộn trang
+window.onscroll = function () {
+  scrollFunction();
+};
+
+// Hiển thị hoặc ẩn nút quay về đầu trang
+function scrollFunction() {
+  const toTopBtn = document.getElementsByClassName('toTopBtn')[0];
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    toTopBtn.style.display = "block";
+  } else {
+    toTopBtn.style.display = "none";
+  }
+}
+
+// Khi người dùng nhấp vào nút, cuộn trang lên đầu
+document.getElementsByClassName('toTopBtn')[0].addEventListener("click", function () {
+  document.body.scrollTop = 0; // Đối với Safari
+  document.documentElement.scrollTop = 0; // Đối với Chrome, Firefox, IE, và Opera
+});
